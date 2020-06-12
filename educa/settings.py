db6ab9ee0637e9c676fab4ad9141f418c17e240e
Nick Mwangemi <nickmwangemi@gmail.com>
2020-06-12 22:07:29 +0300

    Installed and configured Django cache and Memcached cache backend

--- educa/settings.py
+++ educa/settings.py
@@ -49,7 +49,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -138,6 +140,10 @@ CACHES = {
     }
 }
 
+# site cache
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
+CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
 
 
 
