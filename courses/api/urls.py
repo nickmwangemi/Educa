09dd36d13a2aa53bad3c7d88dcc48fef6dfb85ba
Nick Mwangemi <nickmwangemi@gmail.com>
2020-06-15 20:29:13 +0300

    Created viewsets and routers with custom permissions

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
 from . import views
 
 app_name = 'courses'
 
+# api root - takes charge of generating URLs automatically for viewsets
+router = routers.DefaultRouter()
+router.register('courses',views.CourseViewSet)
+
 urlpatterns = [
     path('subjects/',views.SubjectListView.as_view(),name='subject_list'),
     path('subjects/<pk>/',views.SubjectDetailView.as_view(),name='subject_detail'),
-    path('courses/<pk>/enroll/',views.CourseEnrollView.as_view(),name='course_enroll'),
-
-
+    path('',include(router.urls)),
 
 ]
